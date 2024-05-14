7e967cac612ba0ca3c8dc4da963db478040deb9c
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-13 19:31:03 -0500

    Ultimo commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 // Seleccionar contenido 
 
 // querySelector
-const heading = document.querySelector('.header__texto h2');
+const heading = document.querySelector('.header__texto h2'); // Selecciona el primer elemento que coincida con el selector
+heading.textContent = 'Nuevo Heading';
 console.log(heading);
 
 
-
-
-
-
-
 // querySelectorAll
-
+const enlaces = document.querySelectorAll('.navegacion a'); // Selecciona todos los elementos que coincidan con el selector
+console.log(enlaces);
 
 
 
