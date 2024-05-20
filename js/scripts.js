db0384327c68f00f66a69132c2c08f3243461eae
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-20 09:48:44 -0500

    Ultimo commit

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,12 @@ console.log(heading);
 
 // querySelectorAll
 const enlaces = document.querySelectorAll('.navegacion a'); // Selecciona todos los elementos que coincidan con el selector
-console.log(enlaces);
+console.log(enlaces[0]);
 
-
-
-
-
+enlaces[0].textContent = 'Nuevo Texto para Enlace';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
 
 // getElementById
+const heading2 = document.getElementById('heading');
+console.log(heading2);
